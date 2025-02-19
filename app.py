fe577e97fcd1c97180981fade82607d7d18d5ab6
Resul Raveenran <67456014+resulraveendran@users.noreply.github.com>
2025-02-19 12:33:58 +0530

    ci:postgres update in app.py file

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ from functools import wraps
 from flask import Flask, jsonify, Response, request, redirect, url_for, make_response, render_template_string, render_template
 from typing import List, Optional
 import os
-import sqlite3
 from cache import MemoryCache
-import openai
 from vanna.chromadb.chromadb_vector import ChromaDB_VectorStore
 from gevent.pywsgi import WSGIServer
 from vanna.flask import VannaFlaskApp, Cache
@@ -40,70 +38,28 @@ chroma_path = os.getenv("CHROMA_PATH")
 MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
 
 
+
 class MyVanna(ChromaDB_VectorStore, Mistral):
     def __init__(self, config=None):
         ChromaDB_VectorStore.__init__(self, config=config)
         Mistral.__init__(self, config={'api_key': MISTRAL_API_KEY, 'model': model})
 
-    def connect_to_sqlite_multi(self, primary_url: str, additional_urls: list = None):
-        """
-        Connect to a primary SQLite database and optionally attach multiple additional databases.
-
-        Args:
-            primary_url (str): The URL or local path of the primary database to connect to.
-            additional_urls (list, optional): List of URLs or local paths for additional databases to attach.
-
-        Returns:
-            None
-        """
-
-        # Helper function to handle both URLs and local paths
-        def get_database_path(url):
-            if os.path.exists(url):
-                # If it's a local file path, just return it
-                return url
-            else:
-                # If it's a URL, download it
-                path = os.path.basename(urlparse(url).path)
-                if not os.path.exists(path):
-                    response = requests.get(url)
-                    response.raise_for_status()  # Check that the request was successful
-                    with open(path, "wb") as f:
-                        f.write(response.content)
-                return path
-
-        # Helper function to extract alias from the path based on 'DRH'
-        def extract_alias(url):
-            # Extract the part after 'DRH' and before the file extension
-            basename = os.path.basename(url)
-            alias = basename.split('DRH.')[1].split('.')[0]
-            return alias
-
-        # Connect to the primary database
-        primary_path = get_database_path(primary_url)
-        conn = sqlite3.connect(primary_path)
-
-        # Attach additional databases, if provided
-        if additional_urls:
-            for url in additional_urls:
-                additional_path = get_database_path(url)
-                alias = extract_alias(url)  # Generate alias based on content after 'DRH'
-                conn.execute(f"ATTACH DATABASE '{additional_path}' AS {alias}")
-
-        # Define run_sql function for executing queries
-        def run_sql_sqlite(sql: str):
-            return pd.read_sql_query(sql, conn)
-
-        # Set the run_sql function to be used
-        self.run_sql = run_sql_sqlite
-        self.run_sql_is_set = True
-
 
 vn = MyVanna(config={"model": model, "path": chroma_path})
-vn.connect_to_sqlite_multi(
-        primary_url=os.getenv('primary_db_path'),
-        additional_urls=os.getenv('additional_db_path').split(',')
-    )
+db_credentials = {
+    "host": os.getenv("REMOTE_HOST"),
+    "user": os.getenv("REMOTE_UNAME"),
+    "password": os.getenv("REMOTE_PASSWD"),
+    "database": os.getenv("DB_NAME"),
+    "port": os.getenv("PORT"),
+}
+vn.connect_to_postgres(
+    host=db_credentials["host"],
+    dbname=db_credentials["database"],
+    user=db_credentials["user"],
+    password=db_credentials["password"],
+    port=db_credentials["port"],
+)
 
 
 def get_schema_name(env_var: str) -> Optional[str]:
@@ -263,63 +219,10 @@ class CustomVannaFlask(VannaFlaskApp):
                 )
 
 
-app = CustomVannaFlask(vn=vn, cache=MemoryCache())
-memory_cache = MemoryCache()
+app = CustomVannaFlask(vn=vn, cache=cache)
+#memory_cache = MemoryCache()
 flask_app = app.flask_app
 CORS(flask_app)
-
-
-@flask_app.route("/api/v0/ask_question_and_run_query", methods=["POST","OPTIONS"])
-def ask_question_and_run_query():
-    """
-    Ask a question and run the SQL query generated based on the question.
-    ---
-    parameters:
-      - name: question
-        in: body
-        type: string
-        required: true
-        description: The question to generate the SQL query from.
-    responses:
-      200:
-        description: A JSON object containing the SQL query and HTML representation of the result.
-        schema:
-          type: object
-          properties:
-            sql:
-              type: string
-              description: The generated SQL query.
-            df_html:
-              type: string
-              description: The HTML representation of the query result.
-      400:
-        description: Invalid input.
-    """
-    sql = None
-    result = None
-    df_html = None
-    if request.method == "OPTIONS":
-        return '', 200
-    if request.method == "POST":
-        # Retrieve the question from the request body
-        data = request.get_json()
-        question = data.get('question')
-
-        if question:
-            try:
-                sql = vn.generate_sql(question, allow_llm_to_see_data=True)
-                # Run SQL query and fetch result
-                result = vn.run_sql(sql)
-                # Assuming the result is a DataFrame
-                df = pd.DataFrame(result)
-                # Convert the DataFrame to HTML
-                df_html = df.to_html(classes="table table-bordered", index=False)
-
-            except Exception as e:
-                result = "Sorry, I couldn't quite understand your question. Could you please rephrase or provide more details?"
-                return jsonify({'error': result})
-
-    return jsonify({'sql': sql, 'df_html': df_html})
 
 
 if __name__ == '__main__':
